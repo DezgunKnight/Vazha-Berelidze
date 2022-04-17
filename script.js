cc3fb812e688e6ace59176899334265b95a6b311
DezgunKnight <berelidzevaja11@gmail.com>
2022-04-17 19:18:28 +0400

    all done except burger

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ $('.js-anchor-link').click(function(e){
   });
 
 
-  let burgerButton = document.getElementById('toggleButton');
-  let navBar = document.getElementById('navigation-ul');
-  
-  
-  burgerButton.addEventListener('click', function() {
-      navBar.classList.toggle('toggle');
-      burgerButton.classList.toggle('active');
-  })
+  let navBar = document.getElementById('navigation');
+let toggleButton = document.getElementById('burgerBar');
+
+toggleButton.addEventListener('click', function() {
+    navBar.classList.toggle('activenav');
+})
